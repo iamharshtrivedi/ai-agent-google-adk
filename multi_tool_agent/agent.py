c909d3c5597c7205c629dce6a9d4340b6fc048dc
Harsh Trivedi <hktrivedi264@gmail.com>
2025-11-17 17:34:08 +0530

    ADK Updated.

--- multi_tool_agent/agent.py
+++ multi_tool_agent/agent.py
@@ -1,6 +1,6 @@
 import datetime
 from zoneinfo import ZoneInfo
-from google.adk.agents import Agent
+from google.adk.agents.llm_agent import Agent
 
 def get_weather(city: str) -> dict:
     """Retrieves the current weather report for a specified city.
@@ -56,7 +56,7 @@ def get_current_time(city: str) -> dict:
 
 root_agent = Agent(
     name="weather_time_agent",
-    model="gemini-2.0-flash",
+    model="gemini-2.5-flash",
     description=(
         "Agent to answer questions about the time and weather in a city."
     ),
